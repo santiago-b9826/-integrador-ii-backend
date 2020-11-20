09d3d2878053977d8549cbbea7713d946ec0bdf6
Andres Moreno <andresmore06@gmail.com>
2020-11-19 21:11:09 -0500

    Finished transaction CRUD

--- app/api/v1/components/transaction/route.js
+++ app/api/v1/components/transaction/route.js
@@ -7,7 +7,7 @@ router.route('/')
   .post((req, res) => controller.create(req, res));
 
 router.route('/')
-  .get((res) => controller.getAll(res));
+  .get((req,res) => controller.getAll(res));
 
 router.route('/')
   .get((req, res) => controller.getByProjectId(req, res));
